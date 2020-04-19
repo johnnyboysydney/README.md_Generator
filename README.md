5fc58bd56f33a8eebb0a7f900d209b8c1256902f
Johnnyboysydney <54227198+johnnyboysydney@users.noreply.github.com>
2020-04-19 16:45:42 +1000

    Clean up

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # README.md_Generator
 Using Node.js, create a template README.md file with users input to generate the README.
+
+
+[GitHub] (https://www.github.com "Github Home")  <!-- Links -->
+_Testing Italic_ <!-- italic -->
+**Titles**<!-- Bold -->
+
+|Name|Email|Address|                                <!-- Tables -->
+|------|-----|-------|
+|John|john.merchan@gmail.com|Sydney, Australia|
+
+>So he said so    <!-- Quotes -->
+
+So you add the following ``<h1>Title</h1>` and this is code   <!-- Inserting code -->
+
+
+<!-- Code block -->
+```Javascript 
+let express = require("express")
+let app = express();
+...
+```
+<!-- list -->
+
+1. Item 1
+    * Sub Item
+* unordered item
+
+<!-- Adding a line -->
+---
+
+or 
+
+
+*** 
--- index.js
+++ index.js
@@ -3,61 +3,85 @@
 const fs = require("fs");
 const util = require("util");
 const inquirer = require("inquirer");
-//const createReadMe = require('./createReadMe');
+const axios = require("axios");
 
-// 
-function promtUser(){
-    inquirer.prompt([
-        {type: "input",
-        message: "Enter the project title",
-        name: "title",
-        },
-        {type: "input",
-        message: "Enter the project description",
+
+inquirer.prompt([
+    {
+        type: "input",
+        name: "project title",
+        message: "What is the project title?",
+    },
+    {
+        type: "input",
         name: "description",
-        },
-        {type: "input",
-        message: "Enter licence type",
-        name: "licence",
-        },
-        {type: "input",
-        message: "Enter the names of the contributing people to the project",
-        name: "contruibuiting",
-        },
-        {type: "input",
-        message: "Enter your GitHub username",
-        name: "username",
-        }
-        {type: "input",
-        message: "Enter the tests performed",
+        message: "Write a brief description of your project"
+    },
+    {
+        type: "checkbox",
+        message: "Select the contents of your Table of Contents",
+        name: "table of contents",
+        choices: [
+            "Project Title",
+            "Description",
+            "Installation",
+            "Usage",
+            "License",
+            "Contributing",
+            "Tests",
+            "Questions"
+        ]
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Describe the installation process if any",
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Chose the appropiate license for this project",
+        choices: [
+            "Apache License 2.0",
+            "Academic Free License v3.0",
+            "GNU General Public License v3.0",
+            "ISC",
+            "MIT License",
+            "Mozilla Public License 2.0",
+            "Open Software License 3.0",
+            "The Unlicense"
+        ]
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Who are the contributors of this projects?"
+    },
+    {
+        type: "input",
         name: "tests",
-        }
-    ])
-    .then(function(res){
-        const htmlBoiler = ` 
-        <html>
-        <head>
-        </head>
-        <body>
-        <p>Your name: ${res.name} </p>
-        <p>Your current location: ${res.location} </p>
-        <p>LinkedIn: ${res.linkedin} </p>
-        <p>Github: ${res.github} </p>
-        </body>
-        </html>
-        `
-        fs.appendFile('index.html', htmlBoiler, function (err) {
-            if (err) throw err;
-        });
-    })
-}
+        message: "Is there a Test included"
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "What do I do if I have an issue?"
+    }
 
-promtUser();
+]).then(function(data) {
+
+    var filename = data.name.toLowerCase().split(' ').join('') + ".json";
+  
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), function(err) {
+  
+      if (err) {
+        return console.log(err);
+      }
+  
+      console.log("Success!");
+  
+    });
+  });
 
 
-//AS A developer
-
-//I WANT a README generator
-
-//SO THAT I can easily put together a good README for a new project
 
